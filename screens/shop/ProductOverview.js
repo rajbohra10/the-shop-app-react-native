f7ec9b1530fb1d67d2eb7f57f0a3b601a77fc2f4
Raj Bohra <rajbohra@Rajs-MacBook-Pro.local>
2019-09-23 13:16:29 +0530

    project set uop

---
+++ screens/shop/ProductOverview.js
@@ -0,0 +1,12 @@
+ import React from 'react';
+ import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+ const ProductOverview = props => {
+    return <FlatList />
+ };
+
+ const styles = StyleSheet.create({
+
+ });
+
+ export default ProductOverview;
